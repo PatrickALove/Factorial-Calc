d2e5983085b104533986bfe09141dce38e3b018a
PatrickALove <PatrickALove@users.noreply.github.com>
2019-04-16 12:45:57 -0400

    Initial commit

---
+++ Factorial Calc/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFactorialCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.mnuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.lstFactorialResult = New System.Windows.Forms.ListBox()
+        Me.mnuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'mnuStrip
+        '
+        Me.mnuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.mnuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.mnuStrip.Name = "mnuStrip"
+        Me.mnuStrip.Size = New System.Drawing.Size(800, 24)
+        Me.mnuStrip.TabIndex = 0
+        Me.mnuStrip.Text = "Menu Strip"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuClear, Me.mnuExit})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'mnuClear
+        '
+        Me.mnuClear.Name = "mnuClear"
+        Me.mnuClear.Size = New System.Drawing.Size(180, 22)
+        Me.mnuClear.Text = "Clear"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(180, 22)
+        Me.mnuExit.Text = "Exit"
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Font = New System.Drawing.Font("Modern No. 20", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalc.Location = New System.Drawing.Point(290, 49)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(220, 42)
+        Me.btnCalc.TabIndex = 1
+        Me.btnCalc.Text = "Calculate Factorial"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'lstFactorialResult
+        '
+        Me.lstFactorialResult.Font = New System.Drawing.Font("Modern No. 20", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstFactorialResult.FormattingEnabled = True
+        Me.lstFactorialResult.ItemHeight = 24
+        Me.lstFactorialResult.Location = New System.Drawing.Point(290, 109)
+        Me.lstFactorialResult.Name = "lstFactorialResult"
+        Me.lstFactorialResult.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lstFactorialResult.ScrollAlwaysVisible = True
+        Me.lstFactorialResult.Size = New System.Drawing.Size(220, 196)
+        Me.lstFactorialResult.TabIndex = 2
+        '
+        'frmFactorialCalculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lstFactorialResult)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.mnuStrip)
+        Me.MainMenuStrip = Me.mnuStrip
+        Me.Name = "frmFactorialCalculator"
+        Me.Text = "Form1"
+        Me.mnuStrip.ResumeLayout(False)
+        Me.mnuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents mnuStrip As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents mnuClear As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents lstFactorialResult As ListBox
+End Class
---
+++ Factorial Calc/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class frmFactorialCalculator
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        'Uses loops to calculate the factorial and display it
+
+        'Declare Variables
+        Dim strFactorial As String
+        Dim intFactorial As Integer
+        Dim intTotalFact As Integer = 1
+        Dim intCounter As Integer = 1
+        Dim intMax As Integer
+
+
+        'Input variables
+        Dim strInputMessage As String = "Please enter the factorial"
+        Dim strInputHeading As String = "Factorial"
+        Dim strNormalMessage As String = "Enter the factorial you want"
+        Dim strNonNumericError As String = "Error- Please enter a number for the factorial you wish to use"
+        Dim strNegativeError As String = "Error- Please enter a positive number for the factorial you wish to use"
+        Dim strTooHighError As String = "Error- Please enter a number smaller than 12 for the factorial you wish to use"
+
+        'Declare and initialize loop variables
+        Dim strCancelClicked As String = ""
+        Dim intMaxNumberOfEntries As Integer = 12
+        Dim intNumberOfEntries As Integer = 1
+
+        'Calculate the factorial
+        strFactorial = InputBox(strInputMessage, strInputHeading, " ")
+        If IsNumeric(strFactorial) Then
+            intMax = Convert.ToInt32(strFactorial)
+            If intMax > 12 Then
+                MessageBox.Show(strTooHighError)
+                lstFactorialResult.Items.Clear()
+                btnCalc.Enabled = True
+            End If
+            If intMax > 0 Then
+                For intFactorial = 1 To intMax
+                    intNumberOfEntries += 1
+                    intCounter *= intFactorial
+                    lstFactorialResult.Items.Add("!" & intFactorial & "==" & intCounter)
+                Next
+            Else
+                MessageBox.Show(strNegativeError)
+                lstFactorialResult.Items.Clear()
+                btnCalc.Enabled = True
+            End If
+        Else
+            MessageBox.Show(strNonNumericError)
+        End If
+        If intNumberOfEntries <= intMax Then
+            strFactorial = InputBox(strInputMessage, strInputHeading, " ")
+        End If
+        If strFactorial = strCancelClicked Then
+            lstFactorialResult.Items.Clear()
+            btnCalc.Enabled = True
+        End If
+        'disable the button
+        btnCalc.Enabled = False
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles mnuClear.Click
+        ' The mnuClear_Click event clears the ListBox object and also enables the Calculate button
+
+        lstFactorialResult.Items.Clear()
+        btnCalc.Enabled = True
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        ' mnuExit_Click event closes the window and exits the application
+
+        Close()
+    End Sub
+End Class
